7dde2da6e9654269890e6311c962f29df2762e10
Harshit0502 <125523493+Harshit0502@users.noreply.github.com>
2025-08-10 23:03:16 +0530

    Update Binary_search.cpp

--- Binary_search/Binary_search.cpp
+++ Binary_search/Binary_search.cpp
@@ -14,3 +14,18 @@ public:
         return -1;
     }
 };
+
+
+int binary_search_custom(vector<int> &a, int target) {
+    int l = 0, r = sz(a)-1, ans = -1;
+    while(l <= r) {
+        int mid = l + (r-l)/2;
+        if(a[mid] <= target) {
+            ans = mid;
+            l = mid+1;
+        } else {
+            r = mid-1;
+        }
+    }
+    return ans; // last index <= target
+}
