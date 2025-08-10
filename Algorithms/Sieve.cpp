64fc8e04de047e280d24bf18fa82d0b54fd222d2
Harshit0502 <125523493+Harshit0502@users.noreply.github.com>
2025-08-10 23:00:10 +0530

    Create Sieve.cpp

---
+++ Algorithms/Sieve.cpp
@@ -0,0 +1,13 @@
+vector<int> primes;
+vector<bool> isPrime;
+void sieve(int n) {
+    isPrime.assign(n+1, true);
+    isPrime[0] = isPrime[1] = false;
+    for(int i = 2; i*i <= n; i++) {
+        if(isPrime[i]) {
+            for(int j = i*i; j <= n; j += i)
+                isPrime[j] = false;
+        }
+    }
+    for(int i = 2; i <= n; i++) if(isPrime[i]) primes.pb(i);
+}
